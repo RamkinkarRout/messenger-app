bb983095e212f5d570527675144dd8aa29e88dfd
Ram <raamkinkar.rout533@gmail.com>
2022-02-03 11:26:32 +0530

    UseMedia query hook to make drawer responshive

--- src/dashboard/DashboardToggle.js
+++ src/dashboard/DashboardToggle.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button, Drawer, Icon } from "rsuite";
 import Dashboard from ".";
-import { useModelState } from "../misc/custom-hook";
+import { useMediaQuery, useModelState } from "../misc/custom-hook";
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModelState();
+  const isMobile = useMediaQuery("( max-width : 992px )");
 
   return (
     <>
@@ -12,7 +13,7 @@ const DashboardToggle = () => {
         <Icon icon={"dashboard"} />
         <span> Dashboard</span>
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <Dashboard />
       </Drawer>
     </>
--- src/misc/custom-hook.js
+++ src/misc/custom-hook.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export function useModelState(defaultValue = false) {
   const [isOpen, setIsOpen] = useState(defaultValue);
@@ -13,3 +13,23 @@ export function useModelState(defaultValue = false) {
 
   return { isOpen, open, close };
 }
+
+//for responsive SideBar
+
+export const useMediaQuery = (query) => {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
+
+  useEffect(() => {
+    const queryList = window.matchMedia(query);
+    setMatches(queryList.matches);
+
+    const listener = (evt) => setMatches(evt.matches);
+
+    queryList.addListener(listener);
+    return () => queryList.removeListener(listener);
+  }, [query]);
+
+  return matches;
+};
